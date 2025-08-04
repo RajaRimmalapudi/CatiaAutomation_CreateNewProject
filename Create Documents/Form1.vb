acceae193e3db30beeb0b21c03fbcf025454bc80
Raja Rimmalapudi <123992347+RajaRimmalapudi@users.noreply.github.com>
2025-08-04 16:01:49 +0200

    Create Documents files
    Creat Documents.sln is the file that contains the program for creating new Catia projects.
    Form1.vb is the buttons forms that is connected to the create Documents.sln code as the solution.

---
+++ Create Documents/Form1.vb
@@ -0,0 +1,56 @@
+﻿Option Explicit On
+
+Imports INFITF
+Imports MECMOD
+Imports PARTITF
+
+Public Class Form1
+
+    Dim mycatia As INFITF.Application
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        Try
+            mycatia = GetObject(, "CATIA.Application")
+        Catch ex As Exception
+            mycatia = CreateObject("CATIA.Application")
+        End Try
+
+        mycatia.Visible = True
+        mycatia.DisplayFileAlerts = True
+
+        mycatia.Documents.Add("Part")
+        MsgBox("Part create Successfully!!!")
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+
+        Try
+            mycatia = GetObject(, "CATIA.Application")
+        Catch ex As Exception
+            mycatia = CreateObject("CATIA.Application")
+        End Try
+
+        mycatia.Visible = True
+        mycatia.DisplayFileAlerts = True
+
+        mycatia.Documents.Add("Product")
+        MsgBox("Product create Successfully!!!")
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+
+        Try
+            mycatia = GetObject(, "CATIA.Application")
+        Catch ex As Exception
+            mycatia = CreateObject("CATIA.Application")
+        End Try
+
+        mycatia.Visible = True
+        mycatia.DisplayFileAlerts = True
+
+        mycatia.Documents.Add("Drawing")
+        MsgBox("Drawing create Successfully!!!")
+    End Sub
+
+End Class
